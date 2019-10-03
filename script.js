1611228fd4fe6a5cef5ed761d8fc44d749489e00
Stephen Moody <darilligames@gmail.com>
2019-10-03 15:46:44 +0000

    updates

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    var game = document.getElementById("gameArea");
+    game.style=("background-color: black; width: 200px; height: 200px")
+    game.innerHTML = "<p>This is the text</p>";
+    console.log("Hello");
+}
+
+console.log("Not Found");
